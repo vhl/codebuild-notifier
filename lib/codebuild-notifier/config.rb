55870e03b1e6fb318b54ff511fddd0ec613755f9
Adam Alboyadjian <adam@cassia.tech>
2018-11-15 15:23:16 -0200

    Add support for additional channel for whitelisted branch notifications.

--- lib/codebuild-notifier/config.rb
+++ lib/codebuild-notifier/config.rb
@@ -2,18 +2,20 @@ module CodeBuildNotifier
   class Config
     DEFAULT_WHITELIST = %w[master release]
 
-    attr_reader :dynamo_table, :region, :slack_admins,
+    attr_reader :additional_channel, :dynamo_table, :region, :slack_admins,
                 :slack_secret_name, :whitelist_branches
 
     # Configuration values specific to CodeBuild Notifier. CBN_ prefix is
     # used because ENV vars with CODEBUILD_ prefix are reserved for use by AWS.
     def initialize(
+      additional_channel: ENV['CBN_ADDITIONAL_CHANNEL'],
       dynamo_table: ENV['CBN_DYNAMO_TABLE'] || 'branch-build-status',
       region: ENV['CBN_AWS_REGION'] || ENV['AWS_REGION'],
       slack_admins: ENV['CBN_SLACK_ADMIN_USERNAMES'],
       slack_secret_name: ENV['CBN_SLACK_SECRET_NAME'] || 'slack/codebuild',
       whitelist_branches: ENV['CBN_WHITELIST_BRANCHES']
     )
+      @additional_channel = additional_channel
       @dynamo_table = dynamo_table
       @region = region
       @slack_admins = slack_admins&.split(',') || []
--- lib/codebuild-notifier/slack_sender.rb
+++ lib/codebuild-notifier/slack_sender.rb
@@ -11,19 +11,27 @@ module CodeBuildNotifier
 
     def send(message)
       Slack.configure { |slack_config| slack_config.token = app_token }
+      channel = message.additional_channel
+      if channel
+        channel = "##{channel}" unless /\A#/.match?(channel)
+        post_message(message, channel)
+      end
+
       message.recipients.each do |email|
         slack_user_id = find_slack_user(email)
-        next unless slack_user_id
-
-        slack_client.chat_postMessage(
-          as_user: app_is_bot_user?,
-          attachments: [message.payload],
-          channel: slack_user_id
-        )
+        slack_user_id && post_message(message, slack_user_id)
       end
     end
 
-    def admin_send(message)
+    private def post_message(message, channel)
+      slack_client.chat_postMessage(
+        as_user: app_is_bot_user?,
+        attachments: [message.payload],
+        channel: channel
+      )
+    end
+
+    private def admin_send(message)
       config.slack_admins.each do |username|
         username = "@#{username}" unless /\A@/.match?(username)
         slack_client.chat_postMessage(
@@ -34,7 +42,7 @@ module CodeBuildNotifier
       end
     end
 
-    def find_slack_user(email)
+    private def find_slack_user(email)
       lookup_response = slack_client.users_lookupByEmail(email: email)
       lookup_response.user.id
     rescue Slack::Web::Api::Errors::SlackError => e
